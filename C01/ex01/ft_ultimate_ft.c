83436642f7f072c3124caf43c4bc62603159572b
Vitória Teixeira <103860241+vitoriaft@users.noreply.github.com>
2026-04-14 17:03:31 -0300

    Add files via upload

---
+++ C01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: viteixei <viteixei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/11 15:51:35 by viteixei          #+#    #+#             */
+/*   Updated: 2026/04/12 19:24:08 by viteixei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+
+void	ft_ultimate_ft(int *********nbr)
+{
+	 *********nbr = 42;
+}
+
+int	main(void)
+{
+	int	valor = 24;
+	
+	int	*ponteiro1 = &valor;
+	int	**ponteiro2 = &ponteiro1; 
+	int	***ponteiro3 = &ponteiro2;
+	int	****ponteiro4 = &ponteiro3; 
+	int	*****ponteiro5 = &ponteiro4;
+	int	******ponteiro6 = &ponteiro5;
+	int	*******ponteiro7 = &ponteiro6;
+	int	********ponteiro8 = &ponteiro7;
+	int	*********ponteiro9 = &ponteiro8;
+
+	ft_ultimate_ft(ponteiro9);
+	printf("%d", valor);
+	printf("%p", *ponteiro9);
+	return 0;
+}
