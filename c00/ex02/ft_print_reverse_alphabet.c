4927fc459abdf78ca09d0c93db1a94c49878563e
Vitória Teixeira <103860241+vitoriaft@users.noreply.github.com>
2026-04-16 19:57:27 -0300

    Add files via upload

---
+++ c00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: viteixei <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/08 14:14:16 by viteixei          #+#    #+#             */
+/*   Updated: 2026/04/09 14:20:16 by viteixei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	letra;
+
+	letra = 'z';
+	while (letra >= 'a')
+	{
+		write(1, &letra, 1);
+		letra--;
+	}
+}
+/*
+int	main(void)
+{
+	ft_print_reverse_alphabet();
+	return(0);
+}
+*/
